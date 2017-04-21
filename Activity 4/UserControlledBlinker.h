a11d23dadf4337e6b404aec88aeab793faa75cbd
Alexis Lee <AlexisLee1999@gmail.com>
2017-04-21 07:50:26 -0400

    Create UserControlledBlinker.h

---
+++ Activity 4/UserControlledBlinker.h
@@ -0,0 +1,24 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+//Declarations
+  int num;
+  int data = 5;
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0)
+  {
+    data = Serial.parseInt();
+    num = constrain(data, 1, 10);
+  }
+  else
+  {
+    delay(200 * num);
+    digitalWrite(13, HIGH);
+    delay(200 * num);
+    digitalWrite(13, LOW);
+  }
+  
+}
