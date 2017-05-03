cf615616f97fafc7e8ba670f5ab2f5a2633f6b67
Alexis Lee <AlexisLee1999@gmail.com>
2017-05-03 07:46:28 -0400

    Create Nightlight.h

---
+++ Activity 7/Nightlight.h
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(A0);
+  if (sensorValue < 600) // To change the point at which the light turns on change this value.
+  {
+    digitalWrite(13, HIGH);
+  }
+  else 
+  {
+    digitalWrite(13,LOW);
+  }
+}
